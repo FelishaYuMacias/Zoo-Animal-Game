ca892cbbef5bf6af326b9884b2b9c77680728781
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2022-10-11 11:08:00 -0700

    accessed zoo animal API and familiarized ourselves with the data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-console.log ("linked")
-
-
 var cardContainerEl = document.querySelector('#card-container');
 var croppedImg = document.querySelector('#cropped-img');
 var imageContainerEl = document.querySelector('#img-container');
@@ -11,6 +8,23 @@ var imgMargin_left = width / 2 - 50;
 var imgMargin_top = height / 2 - 50;
 croppedImg.setAttribute('style', 'margin-left:-'+imgMargin_left+'px;margin-top:-'+imgMargin_top+'px;');
 
+
+//this is the main function to play our game, it is also where the zoo animal api is accessed
+function playZooGame () {
+    var apiLink = "https://zoo-animal-api.herokuapp.com/animals/rand"
+
+    fetch(apiLink, {
+        cache: "reload",
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+playZooGame();
 cardContainerEl.addEventListener('click', function(event){
     
     var clickedEl = event.target;
