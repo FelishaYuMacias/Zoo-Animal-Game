fa84f71eaa3b37514b23ff96602d230001fb6af6
MoMo R <moran.risk78@gmail.com>
2022-10-11 13:10:32 -0700

    get the data from API and display on html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 var cardContainerEl = document.querySelector('#card-container');
 var croppedImg = document.querySelector('#cropped-img');
 var imageContainerEl = document.querySelector('#img-container');
+var clueTypeEl = document.querySelector('#clue-type');
+var blurImageEl = document.querySelector('#blur-image');
+var clueTypeEl = document.querySelector('#clue-type');
+var clueActiveEl = document.querySelector('#clue-active');
+var clueLengthEl = document.querySelector('#clue-length');
+var clueWeightEl = document.querySelector('#clue-weight');
+var clueHabitatEl = document.querySelector('#clue-habitat');
+var clueDietEl = document.querySelector('#clue-diet');
+var clueGeoEl = document.querySelector('#clue-geo');
+var clueFirstwordEl = document.querySelector('#clue-firstword');
 
 var width = croppedImg.clientWidth;
 var height = croppedImg.clientHeight;
@@ -21,36 +31,42 @@ function playZooGame () {
         })
         .then(function (data) {
             console.log(data);
+            // data.image_link
+
+            blurImageEl.setAttribute('src', data.image_link);
+            clueTypeEl.textContent = data.animal_type;
+            clueActiveEl.textContent = data.active_time;
+            clueLengthEl.textContent = data.length_min + ' - '+ data.length_max + ' ft.';
+            clueWeightEl.textContent = data.weight_min + ' - '+data.weight_max + ' lbs.';
+            clueHabitatEl.textContent = data.habitat;
+            clueDietEl.textContent = data.diet;
+            clueGeoEl.textContent = data.geo_range;
+            croppedImg.setAttribute('src', data.image_link);
+
+            var nameWordArr = data.name.split(" ");
+            if (nameWordArr.length > 1){
+                clueFirstwordEl.textContent = nameWordArr[0];
+            } else {
+                clueFirstwordEl.textContent = 'There is only one word';
+            }
+
         })
 }
 
 playZooGame();
-cardContainerEl.addEventListener('click', function(event){
-    
-    var clickedEl = event.target;
-    console.log(clickedEl);
-    if ( !(clickedEl.matches(".flip-card-front") ) ) return;
 
-    console.log(clickedEl.parentElement.parentElement);
-    var flipCardEl = clickedEl.parentElement.parentElement;
-    console.log(flipCardEl.setAttribute('class', ' flip-card-clicked'));
-})
-
-var blurNum = 30;
+var blurNum = 25;
 
 cardContainerEl.addEventListener('click', function(event){
     
     var clickedEl = event.target;
-    console.log(clickedEl);
+    // console.log(clickedEl);
     if ( !(clickedEl.matches(".flip-card-front") ) ) return;
 
-    console.log(clickedEl.parentElement.parentElement);
-
     var flipCardEl = clickedEl.parentElement.parentElement;
-    console.log(flipCardEl.setAttribute('class', ' flip-card-clicked'));
+    flipCardEl.setAttribute('class', ' flip-card-clicked');
 
     var imgEl = imageContainerEl.children[0].children[0];
-    console.log(imgEl);
 
     blurNum -= 2;
     imgEl.setAttribute('style', 'filter:blur('+blurNum+'px)')
